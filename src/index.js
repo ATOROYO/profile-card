5056652ed1d47f8fb99e89a1a01ea3b339d98899
ATOROYO <davidatoroyosika@gmail.com>
2025-02-18 22:34:22 +0300

    Created the app component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Avatar />
+      <div>
+        <Intro />
+      </div>
+    </div>
+  );
+}
